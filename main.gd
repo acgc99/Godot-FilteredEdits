9306c67c3fa9fc8455256c352ed25cb143417544
acgc99 <acgc99@outlook.es>
2023-08-05 12:09:32 +0200

    closes #17; Bug fix on autocompletion at FilteredTextEdit

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	get_node("FilteredLineEdit").grab_focus()
+#	get_node("FilteredLineEdit").grab_focus()
 #	get_node("FilteredTextEdit").grab_focus()
 #	get_node("FilteredLineEdit").clamp_text()
 	for i in range(get_node("FilteredTextEdit").get_line_count()):
